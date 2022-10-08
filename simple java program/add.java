a025e31d1a63181411dd42a748b3c46b35e2ce0f
navin.kumar <navin.kumar@i-exceed.com>
2022-10-08 19:14:45 +0530

    first commit of intelli J project

---
+++ simple java program/add.java
@@ -0,0 +1,8 @@
+class add{
+public static void main(String... args)
+{
+	int a=100;
+	int b=100;
+	System.out.println(a+b);
+}
+}
---
+++ simple java program/consoleInput.java
@@ -0,0 +1,11 @@
+
+public class consoleInput
+{
+    public static void main(String[] args) 
+    {        
+        // Using Console to input data from user
+        String name = System.console().readLine();
+        
+        System.out.println(name);
+    }
+}
